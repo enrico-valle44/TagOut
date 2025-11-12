c3ce4204eeb46df4ce79335044703cd32936fe8c
ANNA <anna.berti.14-1@accademiadigitaleliguria.it>
2025-11-12 19:34:38 +0100

    Profilo. TODO: delete report

--- frontend/src/app/components/profile/profile.ts
+++ frontend/src/app/components/profile/profile.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, inject  } from '@angular/core';
+import { ReportCard } from '../report-card/report-card';
+import { Report } from '../../model/report';
+import { Utente } from '../../model/utente';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { DataService } from './../../services/data-service/data-service';
+import { LocalStorageService } from '../../services/local-storage-service/local-storage.service';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [ReportCard, MatButtonModule, MatIconModule, RouterLink, CommonModule, MatCardModule, MatChipsModule],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
 export class Profile {
 
+  // servizi (per dopo)
+  private dataServ = inject(DataService);
+  private memoria = inject(LocalStorageService);
+
+  // segnali o proprietà per dati
+  public utente: Utente | null = null;
+  public userReports: Report[] = [];
+
+  constructor() {
+    this.loadProfile();
+  }
+
+  async loadProfile() {
+    const idUser = this.memoria.getId(); //uso servizio di localstorage
+    if (!idUser) {
+      console.log('Nessun id utente nel localStorage.');
+      this.utente = null;
+      this.userReports = [];
+      return;
+    }
+    // se lo trova awaitamente prende i dati utente
+    this.utente = await this.dataServ.getUserInfo(idUser);
+
+    // report dell’utente
+    this.userReports = await this.dataServ.getReportsByUserId(idUser);
+
+    //controlli
+    console.log('Utente:', this.utente);
+    console.log('Report utente:', this.userReports);
+
+  }
+
+  //utile per il caricamento in lista dei report
+  trackByReportId(index: number, report: Report) {
+    return report.id;
+  }
 }
--- frontend/src/app/services/data-service/data-service.ts
+++ frontend/src/app/services/data-service/data-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { FeatureCollection } from 'geojson';
 import { Report } from '../../model/report';
 import { Category } from '../../model/category';
+import { Utente } from '../../model/utente';
 
 @Injectable({
   providedIn: 'root',
@@ -63,4 +64,41 @@ export class DataService {
         throw err;
       });
   }
+
+  //user
+  
+  /** Prende l'id dal localStorage e chiama GET /UserInfo/{id} */
+  getUserInfo(idUser: number): Promise<Utente | null> {
+    return fetch(`http://localhost:5089/UserInfo/${idUser}`)
+      .then((resp) => {
+        if (!resp.ok) {
+          console.error(`Errore HTTP ${resp.status}`);
+          return null;
+        }
+        return resp.json() as Promise<Utente>;
+      })
+      .catch((err) => {
+        console.error('Errore getUserInfo:', err);
+        return null;
+      });
+  }
+
+
+  /** Ritorna i report dell’utente: GET /Report/byUser/{id} (adatta il path al tuo backend) */
+  getReportsByUserId(idUser: number): Promise<Report[]> {
+    return fetch(`http://localhost:5089/Report/user/${idUser}`)
+      .then((resp) => {
+        if (!resp.ok) {
+          console.error(`getReportsByUserId - HTTP ${resp.status}`);
+          return [];
+        }
+        return resp.json() as Promise<Report[]>;
+      })
+      .catch((err) => {
+        console.error('Errore getReportsByUserId:', err);
+        return [];
+      });
+}
+
+
 }
